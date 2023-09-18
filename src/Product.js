2ae5c19f62ca699b670b8c7205939e19764a3a0b
Arif Dikkanat <arifdikkanat2000@gmail.com>
2023-09-18 14:17:17 +0300

    Product Added

--- src/Product.js
+++ src/Product.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product(props) {
     return (
             <div className="product">
                 <div className="product_info">
-                    <p>Title</p>
-                    <p className="product_price"><small>$</small>  <strong>30</strong></p>
-                    <div className="product_rating">* *</div>
+                    <p>{props.title}</p>
+                    <p className="product_price"><small>$</small>  <strong>{props.price}</strong></p>
+                    <div className="product_rating"> {Array(props.rating).fill().map((_,i) => (
+                        <p>⭐</p>
+                    ))}  </div>
                 </div>
-                <img src="https://cdn3.hipicon.com/images/bd/products/2023/05/18/uncommon-istanbul--summer-tote-anta-min--168439717489615.jpg&w=990&h=990" alt="product-image" />
+                <img src={props.image} alt="product-image" />
                 <button>add to basket</button>
             </div>
     );
